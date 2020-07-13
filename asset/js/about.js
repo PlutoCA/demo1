26f2c92c5a40ea6cefc0657339baacd421e4081b
pluto <1031349211@qq.com>
2020-07-13 14:45:39 +0800

    feat: change

--- asset/js/about.js
+++
@@ -1,7 +0,0 @@
-/*
- * @Date: 2019-09-27 17:03:24
- * @LastEditors: Pluto
- * @LastEditTime: 2019-09-27 17:03:24
- */
-
-import "../style/about.scss"
--- asset/js/mobile.js
+++
@@ -1,6 +0,0 @@
-/*
- * @Date: 2019-09-27 18:49:21
- * @LastEditors: Pluto
- * @LastEditTime: 2019-09-27 18:49:54
- */
-import '../style/mobile.scss';
